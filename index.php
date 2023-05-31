342b520540898fd39f00a74fb642097506e1551c
Jurij Gergelaba <mapss@inbox.lv>
2023-06-01 01:09:21 +0300

    php test

--- index.php
+++ index.php
@@ -167,6 +167,8 @@
         </svg>
         <span>VIBRANIUM</span>
         <span style="color:#606A6F">.VC</span>
+
+        <?php echo 'php test';?>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
